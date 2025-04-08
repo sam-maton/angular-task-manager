a8cd600987d2f24fc6ec9e9aa235a629fce65293
Sam Maton <sam.maton91@gmail.com>
2025-04-08 12:17:53 +0100

    finished user component and added tasks component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,12 @@
-import { Component, computed, Input, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  output,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,8 +15,11 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+
+  @Output() selectUser = new EventEmitter<string>();
 
   get imagePath() {
     return `assets/users/${this.avatar}`;
@@ -17,7 +28,11 @@ export class UserComponent {
   // avatar = input.required<string>();
   // name = input.required<string>();
 
+  // selectUser = output<string>();
+
   // imagePath = computed(() => `assets/users/${this.avatar()}`);
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.selectUser.emit(this.id);
+  }
 }
